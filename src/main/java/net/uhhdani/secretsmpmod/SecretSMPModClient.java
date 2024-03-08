25d09c43c91dc6d45d649c65d4ff94c32609ea00
uhhdani <uhhdani@outlook.com>
2024-03-08 05:12:56 +0000

    Initial commit

---
+++ src/main/java/net/uhhdani/secretsmpmod/SecretSMPModClient.java
@@ -0,0 +1,10 @@
+package net.uhhdani.secretsmpmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SecretSMPModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/uhhdani/secretsmpmod/SecretSMPModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.uhhdani.secretsmpmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SecretSMPModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
